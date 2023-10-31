b5a64cc016a6bea48303d4833ea6dbcdb579b09b
Orxyamus <greg@csprousers.org>
2023-10-31 13:16:35 -0400

    added the framework for an Android application (with an icon created by IconKitchen)

---
+++ app/src/main/java/gov/census/cspro/actioninvokerdemo/MainActivity.kt
@@ -0,0 +1,10 @@
+package gov.census.cspro.actioninvokerdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
