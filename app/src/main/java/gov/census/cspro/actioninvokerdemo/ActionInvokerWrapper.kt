252b74206024b8ae8ddcfe64b079b89acbf23d7e
Orxyamus <greg@csprousers.org>
2023-11-01 11:55:45 -0400

    added support for refresh tokens

--- app/src/main/java/gov/census/cspro/actioninvokerdemo/ActionInvokerWrapper.kt
+++ app/src/main/java/gov/census/cspro/actioninvokerdemo/ActionInvokerWrapper.kt
@@ -8,6 +8,7 @@ import androidx.activity.result.contract.ActivityResultContract
 
 
 class ActionInvokerWrapper(var accessToken: String? = null) : ActivityResultContract<String, String>() {
+     private var refreshToken: String? = null
      var title: String = "Action Invoker execution from the Action Invoker Demo"
      var abortOnException: Boolean? = null
 
@@ -16,25 +17,41 @@ class ActionInvokerWrapper(var accessToken: String? = null) : ActivityResultCont
           intent.component = ComponentName("gov.census.cspro.csentry", "gov.census.cspro.ActionInvokerActivity")
 
           // one or more actions are specified by defining the actions in JSON format
-          intent.putExtra("ACTION", input)
+          intent.putExtra(ACTION, input)
 
           // the optional title is displayed by CSEntry when running the actions
-          intent.putExtra("TITLE", title)
+          intent.putExtra(TITLE, title)
 
           // additional optional arguments; by default, CSEntry aborts on the first exception
-          intent.putExtra("ACCESS_TOKEN", accessToken)
-          intent.putExtra("ABORT_ON_EXCEPTION", abortOnException)
+          intent.putExtra(ACCESS_TOKEN, accessToken)
+          intent.putExtra(ABORT_ON_EXCEPTION, abortOnException)
+
+          // add a refresh token if we have one
+          intent.putExtra(REFRESH_TOKEN, refreshToken)
 
           return intent
      }
 
      override fun parseResult(resultCode: Int, intent: Intent?): String {
-          if( resultCode == Activity.RESULT_OK ) {
-               intent?.getStringExtra("RESULT")?.let {
+          if( resultCode == Activity.RESULT_OK && intent != null ) {
+               // if the user manually approved access to the Action Invoker, a refresh token
+               // is provided that can be used for future calls
+               refreshToken = intent.getStringExtra(REFRESH_TOKEN)
+
+               intent.getStringExtra(RESULT)?.let {
                     return it
                }
           }
 
           return "{\"type\":\"exception\",\"value\":\"Unexpected result from the Action Invoker.\"}"
+     }
+
+     private companion object {
+          const val ABORT_ON_EXCEPTION = "ABORT_ON_EXCEPTION"
+          const val ACCESS_TOKEN = "ACCESS_TOKEN"
+          const val ACTION = "ACTION"
+          const val REFRESH_TOKEN = "REFRESH_TOKEN"
+          const val RESULT = "ABORT_ON_EXCEPTION"
+          const val TITLE = "TITLE"
      }
 }
